028d77cfcbbe63e4c4bcddcf2e61d39d0460d22f
Frederic Lopes Goncalves Magalhaes <fredericlopesgoncalvesmagalhaes@dsl-hkibrasgw1-58c382-181.dhcp.inet.fi>
2016-10-07 17:58:04 +0300

    Starting Software Engineering Project

---
+++ package/main.py
@@ -0,0 +1,10 @@
+#Software Engineering Project
+#Machining Learning - Gathering behaviour in an indoor environment
+#Frédéric Lopes Gonçalves Magalhaes (a1602054) & Nicolas Delbiaggio (a1602052)
+#2016-2017, semester 5
+
+def main():
+    print("Hello World!")
+
+if __name__ == "__main__" :
+    main() 
---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+setup(name='machininglearning_haagahelia2016',
+    version='1.0',
+    packages = ['chardet'],
+    author="Frédéric Lopes Goncalves Magalhaes & Nicolas Delbiaggio",
+    author_email="mag.frederic@gmail.com & mdelbiaggio@gmail.com",
+    description="Machining learning in an indoor environment",
+    long_description="The final product as to gather the behaviour of people in an indoor environment and generate time-slots which will automate some items of the indoor environment (e.g. turn on/off lights, close de main door). In order to achieve it goal, an algorithm is generating time-slots each 7 days."
+)
