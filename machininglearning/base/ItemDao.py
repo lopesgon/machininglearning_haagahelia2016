026f6526df0203214aa5ffc40dae271f622f66eb
Frederic Lopes Goncalves Magalhaes <fredericlopesgoncalvesmagalhaes@dsl-hkibrasgw1-58c382-181.dhcp.inet.fi>
2016-10-16 21:41:02 +0300

    Modification of Item status and creation of StateActionDao.

--- machininglearning/base/ItemDao.py
+++ machininglearning/base/ItemDao.py
@@ -2,22 +2,21 @@ from machininglearning.gatherbehavior import Item
 
 DATA_ITEM = "dataItems.csv"
 
-class ItemDao:
+@staticmethod
+def readItem():
+    global lstItems
+    lstItems = []
+    fichier = open(DATA_ITEM, "r")
+    items = fichier.read()
+    tab = items.splitlines()
+    for lineItem in tab:
+        item = _readLine(lineItem)
+        if item not in lstItems:
+            lstItems.append(item)
+            print(item.name)
+    fichier.close()
 
-    @staticmethod
-    def readItem():
-        global lstItems
-        lstItems = []
-        fichier = open(DATA_ITEM, "r")
-        items = fichier.read()
-        tab = items.splitlines()
-        for lineItem in tab:
-            item = _readLine(lineItem)
-            if item not in lstItems:
-                lstItems.append(item)
-                print(item.name)
-        fichier.close()
-
+@staticmethod
 def _readLine(lineItem):
     lstFeatures = lineItem.split(";")
     no = lstFeatures[0]
